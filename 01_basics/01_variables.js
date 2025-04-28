c2986d80264faebbdfa7161c31768b988b82d8b6
Somkoel <som.koel@gmail.com>
2025-04-28 10:05:34 +0000

    variables learned

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1444552
+let accountEmail = "koel@google.com"
+var accountPassword = 12345
+accountCity = "Kolkata"
+
+// accountId = 2 // not allowed
+accountEmail = "ks@ks.com"
+accountPassword = 12345566
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
